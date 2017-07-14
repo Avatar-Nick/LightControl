9347d062289205b4d85ad065042aa6981506b9e3
RoboAvatar <RoboAvatar65@gmail.com>
2017-07-14 19:33:04 -0400

    Uploading Control Files

---
+++ AutomaticBlinkingLights/AutomaticBlinkingLights.ino
@@ -0,0 +1,100 @@
+/********************************************************************
+ *AutomaticBlinkingLights.ino Automatic Light Control File          *
+ *Witten by Nicholas Maselli                                        *
+ *                                                                  *
+ *Purpose: This program reads in analog microphone data and         *
+ *pulses the relay coresponding to the sound frequency.             *
+ *                                                                  *
+ *Version: 1.0                                                      *
+ *Date: July 14th, 2017                                             *
+ ********************************************************************/
+
+/* FHT default values*/ 
+#define LOG_OUT 1 // use the log output function
+#define FHT_N 256 // set to 256 point fht
+#define OCTAVE 1
+#define OCT_NORM 0
+
+/*Delay sets the number of cycles between updates. Each cycle is
+ around 3 milliseconds*/
+#define DELAY 15
+#define THRESHOLD 50
+
+int numberOfRelays = 8;
+int relay[] = {2, 3, 4, 5, 6, 7, 8, 9};
+int  bias[] = {75, 70, 70, 100, 95, 60, 40, 30};
+
+/*FHT.h library from http://wiki.openmusiclabs.com/wiki/ArduinoFHT */
+#include <FHT.h> 
+
+void setup() {
+  /*Initialize Relay*/
+  for (int i = 0; i < numberOfRelays; i++) {
+     digitalWrite(relay[i], HIGH);
+     pinMode(relay[i], OUTPUT);
+  }
+  
+  /*Example code from FHT library*/
+  Serial.begin(115200); // use the serial port
+  TIMSK0 = 0; // turn off timer0 for lower jitter
+  ADCSRA = 0xe5; // set the adc to free running mode
+  ADMUX = 0x40; // use adc0
+  DIDR0 = 0x01; // turn off the digital input for adc0
+}
+
+void loop() {
+
+  int loopNum = 0;
+
+  /*Example code from FHT library*/
+  while(1) { // reduces jitter
+    cli();  // UDRE interrupt slows this way down on arduino1.0
+    for (int i = 0 ; i < FHT_N ; i++) { // save 256 samples
+      while(!(ADCSRA & 0x10)); // wait for adc to be ready
+      ADCSRA = 0xf5; // restart adc
+      byte m = ADCL; // fetch adc data
+      byte j = ADCH;
+      int k = (j << 8) | m; // form into an int
+      k -= 0x0200; // form into a signed int
+      k <<= 6; // form into a 16b signed int
+      fht_input[i] = k; // put real data into bins
+    }
+    fht_window(); // window the data for better frequency response
+    fht_reorder(); // reorder the data before doing the fht
+    fht_run(); // process the data in the fht
+    fht_mag_octave(); // take the output of the fht
+    sei();
+  /*Example code ends*/
+
+    //Loop to print frequency output and blink lights
+    if (loopNum % DELAY == 0) {
+      for (int i = 0; i < 8; ++i) {
+        Serial.print(fht_oct_out[i] - bias[i]);
+        Serial.print(" ");
+      }
+      Serial.println("");
+
+      //Blink lights depending on frequency threshold
+      for (int i = 0; i <numberOfRelays; i++) {
+        blinkLights(i, fht_oct_out[i] - bias[i]);
+      }
+    }
+    
+    ++loopNum;
+  }
+}
+
+/*Blinks given light strand if value is greater than the treshold*/
+void blinkLights(int strand, int value) {
+  if (value < 0) {
+    value = 0;
+  }
+
+  //Determines wether or not to blink light strand
+  if (value > THRESHOLD) {
+    digitalWrite(relay[strand], LOW);
+  } 
+  else if (value < THRESHOLD) {
+    digitalWrite(relay[strand], HIGH);     
+  }
+}
---
+++ ManualBlinkingLights/ManualBlinkingLights.ino
@@ -0,0 +1,472 @@
+/********************************************************************
+ *ManualBlinkingLights.ino Manual Light Control File                *
+ *Witten by Nicholas Maselli                                        *
+ *                                                                  *
+ *Purpose: This program pulses the relay coresponding to specific   *
+ *manual time delay to the song Carol of Bells. Light syncs to      *
+ *other songs can be created using a similar format.                *
+ *                                                                  *
+ *Version: 1.0                                                      *
+ *Date: July 14th, 2017                                             *
+ ********************************************************************/
+
+/*Note delay is the tempo of the song in milliseconds*/
+int noteDelay = 340;
+int numberOfRelays = 8;
+int relay[] = {2, 3, 4, 5, 6, 7, 8, 9};
+boolean lights[] = {false, false, false, false, false, false, false, false};
+
+void setup() {
+  Serial.begin(9600);  
+  for (int i = 0; i < numberOfRelays; i++) {
+     digitalWrite(relay[i], HIGH);
+     pinMode(relay[i], OUTPUT);
+  }
+
+  /*Type "start" (without quotes) into the serial input to begin in 7 seconds*/
+  while(Serial.readString() != "start") {
+  }
+  delay(7000);
+  song();
+}
+
+void loop() {
+  
+}
+
+void rythme1() {
+  lights[2] = true;
+  blinkLights(lights);
+  lights[2] = false;
+  delay(noteDelay);
+  
+  lights[1] = true;
+  blinkLights(lights);
+  lights[1] = false;
+  delay(noteDelay/2);
+
+  lights[2] = true;
+  blinkLights(lights);
+  lights[2] = false;
+  delay(noteDelay/2);
+  
+  lights[0] = true;
+  blinkLights(lights);
+  lights[0] = false;
+  delay(noteDelay);  
+}
+
+void rythme2() {
+  lights[6] = true;
+  lights[7] = true;
+  blinkLights(lights);
+  lights[6] = false;
+  lights[7] = false;
+  delay(noteDelay);
+  
+  lights[4] = true;
+  lights[5] = true;
+  blinkLights(lights);
+  lights[4] = false;
+  lights[5] = false;
+  delay(noteDelay/2);
+
+  lights[6] = true;
+  lights[7] = true;
+  blinkLights(lights);
+  lights[6] = false;
+  lights[7] = false;
+  delay(noteDelay/2); 
+  
+  lights[2] = true;
+  lights[3] = true;
+  blinkLights(lights);
+  lights[2] = false;
+  lights[3] = false;
+  delay(noteDelay);
+}
+
+void rythme3() {
+  lights[4] = true;
+  lights[5] = true;
+  blinkLights(lights);
+  lights[4] = false;
+  lights[5] = false;
+  delay(noteDelay);
+  
+  lights[2] = true;
+  lights[3] = true;
+  blinkLights(lights);
+  lights[2] = false;
+  lights[3] = false;
+  delay(noteDelay/2);
+
+  lights[4] = true;
+  lights[5] = true;
+  blinkLights(lights);
+  lights[4] = false;
+  lights[5] = false;
+  delay(noteDelay/2);
+  
+  lights[0] = true;
+  lights[1] = true;
+  blinkLights(lights);
+  lights[0] = false;
+  lights[1] = false;
+  delay(noteDelay);  
+}
+
+void power1() {  
+
+  lights[7] = true;
+  blinkLights(lights);
+  lights[7] = false;
+  delay(noteDelay);
+
+  blinkLights(lights);
+  delay(noteDelay/2);
+  
+  lights[7] = true;
+  blinkLights(lights);
+  lights[7] = false;
+  delay(noteDelay/2);
+
+  lights[6] = true;
+  blinkLights(lights);
+  lights[6] = false;
+  delay(noteDelay/2);
+
+  blinkLights(lights);
+  delay(noteDelay/2);
+
+  /*****************/
+  lights[5] = true;
+  blinkLights(lights);
+  lights[5] = false;
+  delay(noteDelay);
+
+  blinkLights(lights);
+  delay(noteDelay/2);
+  
+  lights[5] = true;
+  blinkLights(lights);
+  lights[5] = false;
+  delay(noteDelay/2);
+
+  lights[4] = true;
+  blinkLights(lights);
+  lights[4] = false;
+  delay(noteDelay/2);
+
+  lights[3] = true;
+  blinkLights(lights);
+  lights[3] = false;
+  delay(noteDelay/2);
+
+  /*****************/
+  lights[4] = true;
+  blinkLights(lights);
+  lights[4] = false;
+  delay(noteDelay);
+
+  blinkLights(lights);
+  delay(noteDelay/2);
+  
+  lights[4] = true;
+  blinkLights(lights);
+  lights[4] = false;
+  delay(noteDelay/2);
+
+  lights[5] = true;
+  blinkLights(lights);
+  lights[5] = false;
+  delay(noteDelay/2);
+
+  lights[4] = true;
+  blinkLights(lights);
+  lights[4] = false;
+  delay(noteDelay/2);
+  
+  /*****************/
+  lights[2] = true;
+  blinkLights(lights);
+  lights[2] = false;
+  delay(noteDelay);
+  
+  lights[1] = true;
+  blinkLights(lights);
+  lights[1] = false;
+  delay(noteDelay/2);
+
+  lights[2] = true;
+  blinkLights(lights);
+  lights[2] = false;
+  delay(noteDelay/2);
+  
+  lights[0] = true;
+  blinkLights(lights);
+  lights[0] = false;
+  delay(noteDelay);
+}
+
+void power2() {
+  lights[0] = true;
+  lights[1] = true;  
+  blinkLights(lights);
+  lights[0] = false;
+  lights[1] = false;  
+  delay(noteDelay/2);
+
+  lights[1] = true;
+  lights[2] = true;  
+  blinkLights(lights);
+  lights[1] = false;
+  lights[2] = false;  
+  delay(noteDelay/2);
+
+  lights[2] = true;
+  lights[3] = true;  
+  blinkLights(lights);
+  lights[2] = false;
+  lights[3] = false;  
+  delay(noteDelay/2);
+
+  lights[3] = true;
+  lights[4] = true;  
+  blinkLights(lights);
+  lights[3] = false;
+  lights[4] = false;  
+  delay(noteDelay/2);
+  
+  lights[4] = true;
+  lights[5] = true;  
+  blinkLights(lights);
+  lights[4] = false;
+  lights[5] = false;  
+  delay(noteDelay/2);
+
+  lights[5] = true;
+  lights[6] = true;  
+  blinkLights(lights);
+  lights[5] = false;
+  lights[6] = false;  
+  delay(noteDelay/2);
+
+  lights[6] = true;
+  lights[7] = true;  
+  blinkLights(lights);
+  lights[6] = false;
+  lights[7] = false;  
+  delay(noteDelay/2);
+  
+  lights[7] = true;  
+  blinkLights(lights);
+  lights[7] = false;
+  delay(noteDelay/2);
+
+  
+  lights[2] = true;
+  lights[3] = true;
+  lights[4] = true;
+  lights[5] = true;
+  blinkLights(lights);
+  lights[2] = false;
+  lights[3] = false;
+  lights[4] = false;
+  lights[5] = false;
+  delay(noteDelay);
+  
+  lights[0] = true;
+  lights[1] = true;
+  lights[2] = true;
+  lights[3] = true;
+  lights[4] = true;
+  lights[5] = true;
+  lights[6] = true;
+  lights[7] = true;
+  blinkLights(lights);
+  lights[0] = false;
+  lights[1] = false;
+  lights[2] = false;
+  lights[3] = false;
+  lights[4] = false;
+  lights[5] = false;
+  lights[6] = false;
+  lights[7] = false;
+  delay(noteDelay);    
+}
+
+void full1() {
+  lights[3] = true;
+  lights[4] = true;
+  blinkLights(lights);
+  lights[3] = false;
+  lights[4] = false;
+  delay(noteDelay*3);  
+  
+  lights[2] = true;
+  lights[3] = true;
+  lights[4] = true;
+  lights[5] = true;
+  blinkLights(lights);
+  lights[2] = false;
+  lights[3] = false;
+  lights[4] = false;
+  lights[5] = false;
+  delay(noteDelay*3);  
+  
+  lights[1] = true;
+  lights[2] = true;
+  lights[3] = true;
+  lights[4] = true;
+  lights[5] = true;
+  lights[6] = true;
+  blinkLights(lights);
+  lights[1] = false;
+  lights[2] = false;
+  lights[3] = false;
+  lights[4] = false;
+  lights[5] = false;
+  lights[6] = false;
+  delay(noteDelay*3);  
+  
+  lights[0] = true;
+  lights[1] = true;
+  lights[2] = true;
+  lights[3] = true;
+  lights[4] = true;
+  lights[5] = true;
+  lights[6] = true;
+  lights[7] = true;
+  blinkLights(lights);
+  lights[0] = false;
+  lights[1] = false;
+  lights[2] = false;
+  lights[3] = false;
+  lights[4] = false;
+  lights[5] = false;
+  lights[6] = false;
+  lights[7] = false;
+  delay(noteDelay*3);  
+}
+
+void final1() {
+  lights[3] = true;
+  lights[4] = true;
+  lights[5] = true;
+  blinkLights(lights);
+  lights[3] = false;
+  lights[4] = false;
+  lights[5] = false;
+  delay(noteDelay*3); 
+
+  lights[0] = true;
+  lights[1] = true;
+  lights[2] = true;
+  blinkLights(lights);
+  lights[0] = false;
+  lights[1] = false;
+  lights[2] = false;
+  delay(noteDelay*3); 
+
+  lights[3] = true;
+  lights[4] = true;
+  lights[5] = true;
+  blinkLights(lights);
+  lights[3] = false;
+  lights[4] = false;
+  lights[5] = false;
+  delay(noteDelay*3); 
+
+  lights[5] = true;
+  lights[6] = true;
+  lights[7] = true;
+  blinkLights(lights);
+  lights[5] = false;
+  lights[6] = false;
+  lights[7] = false;
+  delay(noteDelay*3); 
+  
+}
+
+void final2() {
+  delay(noteDelay*3); 
+  lights[2] = true;
+  lights[3] = true;
+  lights[4] = true;
+  lights[5] = true;
+  blinkLights(lights);
+  lights[2] = false;
+  lights[3] = false;
+  lights[4] = false;
+  lights[5] = false;
+  delay(noteDelay*9);
+
+  lights[0] = true;
+  lights[1] = true;
+  lights[2] = true;
+  lights[3] = true;
+  lights[4] = true;
+  lights[5] = true;
+  lights[6] = true;
+  lights[7] = true;
+  blinkLights(lights);
+  lights[0] = false;
+  lights[1] = false;
+  lights[2] = false;
+  lights[3] = false;
+  lights[4] = false;
+  lights[5] = false;
+  lights[6] = false;
+  lights[7] = false;  
+}
+
+/*Blinks to the song Carol of Bells from the YouTube
+  video https://www.youtube.com/watch?v=JZkvL0snS7w */
+void song() {  
+  for (int i = 0; i < 16; i++)  {
+    rythme1();
+  }
+  
+  for (int i = 0; i < 4; i++)  {
+    rythme2();
+  }
+  
+  power1();
+  power2();
+  power2();
+
+  for (int i = 0; i < 24; i++) {
+    rythme3();
+  }
+
+  for (int i = 0; i < 4; i++)  {
+    rythme2();
+  }
+  
+  power1();
+  power2();
+  power2();
+
+  for (int i = 0; i < 8; i++) {
+    rythme3();
+  }
+
+  full1();
+  final1();
+  final2();
+}
+
+/*Change the state of input lights*/
+void blinkLights(boolean lights[]) {
+  for (int i = 0; i < numberOfRelays; i++) {
+    if (lights[i] == false) {
+      digitalWrite(relay[i], HIGH);
+      continue;
+    }
+    
+    if (lights[i] == true) {
+      digitalWrite(relay[i], LOW);
+    }
+  }
+}
